75d5ba52c41f32044505c5d8a13ef5e9da46a1d0
Cale <cale.short@gmail.com>
2017-02-14 15:44:25 -0500

    remove growPop function

--- RabbitPopulation.hs
+++ RabbitPopulation.hs
@@ -1,5 +1,7 @@
 module RabbitPopulation (progressTime) where
 
+import Data.List (sort)
+
 -- Data
 
 type Age        = Int
@@ -24,16 +26,11 @@ numberAdults pairs = length $ filter (\(RabbitPair age) -> age > 0) pairs
 ageRabbits :: [RabbitPair] -> [RabbitPair]
 ageRabbits = map (\(RabbitPair age) -> RabbitPair (age + 1))
 
-growPop :: GrowthRate -> [RabbitPair] -> [RabbitPair]
-growPop growthRate pairs = 
-  (makeBabies (growthRate * (numberAdults pairs)))
-
 progressTime :: Months -> GrowthRate -> [RabbitPair] -> [RabbitPair]
 progressTime 1      growthRate rabbits = rabbits
 progressTime months growthRate rabbits =
   progressTime (months - 1) growthRate ((ageRabbits rabbits) ++ newborns)
-    where newborns = growPop growthRate rabbits
+    where newborns = makeBabies (growthRate * (numberAdults rabbits))
 
 main = do
-  print $ progressTime 6 1 [(RabbitPair 0)] 
-  -- Ouput matches the image here: https://cdn-images-1.medium.com/max/600/1*S8NqTJkpoIFrXH4gzyrDsQ.png
+  print $ progressTime 6 1 [(RabbitPair 0)]
