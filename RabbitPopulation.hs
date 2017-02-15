e6e4412681a30645bad41f540dc1c39386f65d40
Cale <cale.short@gmail.com>
2017-02-15 08:42:13 -0500

    Sped up the program using BangPatterns
    Per siddhu_'s recommendation

--- RabbitPopulation.hs
+++ RabbitPopulation.hs
@@ -1,36 +1,28 @@
-module RabbitPopulation (progressTime) where
-
-import Data.List (sort)
-
--- Data
-
+{-# LANGUAGE BangPatterns #-}
+ 
+module Main where
+ 
 type Age        = Int
 type Months     = Int
 type GrowthRate = Int
-
-data RabbitPair = RabbitPair Age deriving (Eq, Ord)
-
-instance Show RabbitPair where
-   show (RabbitPair age) = 
-     if age > 0 then "rr" ++ show age else "bb" ++ show age
-
--- Operations
-
-makeBabies :: Int -> [RabbitPair]
-makeBabies 0         = []
-makeBabies numToMake = (RabbitPair 0) : makeBabies (numToMake - 1)
-
-numberAdults :: [RabbitPair] -> Int
-numberAdults pairs = length $ filter (\(RabbitPair age) -> age > 0) pairs
-
-ageRabbits :: [RabbitPair] -> [RabbitPair]
-ageRabbits = map (\(RabbitPair age) -> RabbitPair (age + 1))
-
-progressTime :: Months -> GrowthRate -> [RabbitPair] -> [RabbitPair]
-progressTime 1      growthRate rabbits = rabbits
-progressTime months growthRate rabbits =
-  progressTime (months - 1) growthRate ((ageRabbits rabbits) ++ newborns)
-    where newborns = makeBabies (growthRate * (numberAdults rabbits))
-
+ 
+type ReproducingRabbits    = Int
+type NonReproducingRabbits = Int
+ 
+data RabbitColony = RabbitColony !ReproducingRabbits !NonReproducingRabbits
+  deriving (Eq, Ord, Show)
+ 
+progressTime :: Months -> GrowthRate -> RabbitColony -> RabbitColony
+progressTime 1 !growthRate rabbits = rabbits
+progressTime !months !growthRate (RabbitColony reproducing nonReproducing) =
+    progressTime (pred months) growthRate newColony
+  where
+    newColony = RabbitColony (reproducing+nonReproducing) numberOfBabies
+    numberOfBabies = growthRate * reproducing
+ 
+grabResult :: RabbitColony -> Int
+grabResult (RabbitColony n _) = n
+ 
 main = do
-  print $ progressTime 6 1 [(RabbitPair 0)]
+  print $ grabResult $ progressTime 5 3 (RabbitColony 1 0)
+  print $ grabResult $ progressTime 32 5 (RabbitColony 1 0)
